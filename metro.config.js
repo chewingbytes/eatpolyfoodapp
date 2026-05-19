73b3f680e0474c330b04f04635266a36b19a7e7f
chewingbytes <bryanchewzy24@gmail.com>
2026-05-19 14:42:54 +0800

    fix hermes dynamic import error from opentelemetry

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,12 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
+// Hermes can't parse dynamic import() with webpack magic comments (e.g. /* webpackIgnore: true */)
+// used by @sanity/client's OpenTelemetry deps. Force Metro/Babel to transpile them.
+config.transformer.transformIgnorePatterns = [
+  "node_modules/(?!(react-native|@react-native|@react-native-community|expo|@expo|@expo-google-fonts|react-navigation|@react-navigation|nativewind|react-native-css-interop|@opentelemetry|@sanity)/)",
+];
+
 module.exports = withNativeWind(config, { input: "./global.css" });
+
+
